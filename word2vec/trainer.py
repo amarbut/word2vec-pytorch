7ab48b5427065743564a16eb604750f2115968e6
Anna Marbut <anna.marbut@submittable.com>
2021-05-26 10:32:53 -0600

    Adapt for sagemaker

--- word2vec/trainer.py
+++ word2vec/trainer.py
@@ -1,15 +1,158 @@
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from tqdm import tqdm
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import init
+import argparse
+import os
+import json
+import sagemaker_containers
+import numpy as np
 
-from word2vec.data_reader import DataReader, Word2vecDataset
-from word2vec.model import SkipGramModel
+class DataReader:
+    NEGATIVE_TABLE_SIZE = 1e8
+
+    def __init__(self, inputFileName, min_count):
+
+        self.negatives = []
+        self.discards = []
+        self.negpos = 0
+
+        self.word2id = dict()
+        self.id2word = dict()
+        self.sentences_count = 0
+        self.token_count = 0
+        self.word_frequency = dict()
+
+        self.inputFileName = inputFileName
+        self.read_words(min_count)
+        self.initTableNegatives()
+        self.initTableDiscards()
+
+    def read_words(self, min_count):
+        word_frequency = dict()
+        for line in open(self.inputFileName, encoding="utf8"):
+            line = line.split()
+            if len(line) > 1:
+                self.sentences_count += 1
+                for word in line:
+                    if len(word) > 0:
+                        self.token_count += 1
+                        word_frequency[word] = word_frequency.get(word, 0) + 1
+
+                        if self.token_count % 1000000 == 0:
+                            print("Read " + str(int(self.token_count / 1000000)) + "M words.")
+
+        wid = 0
+        for w, c in word_frequency.items():
+            if c < min_count:
+                continue
+            self.word2id[w] = wid
+            self.id2word[wid] = w
+            self.word_frequency[wid] = c
+            wid += 1
+        print("Total embeddings: " + str(len(self.word2id)))
+
+    def initTableDiscards(self):
+        t = 0.0001
+        f = np.array(list(self.word_frequency.values())) / self.token_count
+        self.discards = np.sqrt(t / f) + (t / f)
+
+    def initTableNegatives(self):
+        pow_frequency = np.array(list(self.word_frequency.values())) ** 0.5
+        words_pow = sum(pow_frequency)
+        ratio = pow_frequency / words_pow
+        count = np.round(ratio * DataReader.NEGATIVE_TABLE_SIZE)
+        for wid, c in enumerate(count):
+            self.negatives += [wid] * int(c)
+        self.negatives = np.array(self.negatives)
+        np.random.shuffle(self.negatives)
+
+    def getNegatives(self, target, size):  # TODO check equality with target
+        response = self.negatives[self.negpos:self.negpos + size]
+        self.negpos = (self.negpos + size) % len(self.negatives)
+        if len(response) != size:
+            return np.concatenate((response, self.negatives[0:self.negpos]))
+        return response
+
+
+# -----------------------------------------------------------------------------------------------------------------
+
+class Word2vecDataset(Dataset):
+    def __init__(self, data, window_size):
+        self.data = data
+        self.window_size = window_size
+        self.input_file = open(data.inputFileName, encoding="utf8")
+
+    def __len__(self):
+        return self.data.sentences_count
+
+    def __getitem__(self, idx):
+        while True:
+            line = self.input_file.readline()
+            if not line:
+                self.input_file.seek(0, 0)
+                line = self.input_file.readline()
+
+            if len(line) > 1:
+                words = line.split()
+
+                if len(words) > 1:
+                    word_ids = [self.data.word2id[w] for w in words if
+                                w in self.data.word2id and np.random.rand() < self.data.discards[self.data.word2id[w]]]
+
+                    boundary = np.random.randint(1, self.window_size)
+                    return [(u, v, self.data.getNegatives(v, 5)) for i, u in enumerate(word_ids) for j, v in
+                            enumerate(word_ids[max(i - boundary, 0):i + boundary]) if u != v]
+
+    @staticmethod
+    def collate(batches):
+        all_u = [u for batch in batches for u, _, _ in batch if len(batch) > 0]
+        all_v = [v for batch in batches for _, v, _ in batch if len(batch) > 0]
+        all_neg_v = [neg_v for batch in batches for _, _, neg_v in batch if len(batch) > 0]
+
+        return torch.LongTensor(all_u), torch.LongTensor(all_v), torch.LongTensor(all_neg_v)
+
+#----------------------------------------------------------------------------------------------------------------
+
+class SkipGramModel(nn.Module):
+
+    def __init__(self, emb_size, emb_dimension):
+        super(SkipGramModel, self).__init__()
+        self.emb_size = emb_size
+        self.emb_dimension = emb_dimension
+        self.u_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
+        self.v_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
+
+        initrange = 1.0 / self.emb_dimension
+        init.uniform_(self.u_embeddings.weight.data, -initrange, initrange)
+        init.constant_(self.v_embeddings.weight.data, 0)
+
+    def forward(self, pos_u, pos_v, neg_v):
+        emb_u = self.u_embeddings(pos_u)
+        emb_v = self.v_embeddings(pos_v)
+        emb_neg_v = self.v_embeddings(neg_v)
+
+        score = torch.sum(torch.mul(emb_u, emb_v), dim=1)
+        score = torch.clamp(score, max=10, min=-10)
+        score = -F.logsigmoid(score)
+
+        neg_score = torch.bmm(emb_neg_v, emb_u.unsqueeze(2)).squeeze()
+        neg_score = torch.clamp(neg_score, max=10, min=-10)
+        neg_score = -torch.sum(F.logsigmoid(-neg_score), dim=1)
+
+        return torch.mean(score + neg_score)
+
+    def save_embedding(self, file_name):
+        torch.save(self.cpu().state_dict(), file_name)
+
+#-------------------------------------------------------------------------------------------------------
 
 
 class Word2VecTrainer:
-    def __init__(self, input_file, output_file, emb_dimension=100, batch_size=32, window_size=5, iterations=3,
-                 initial_lr=0.001, min_count=12):
+    def __init__(self, input_file, output_file, emb_dimension, batch_size, window_size, iterations,
+                 initial_lr, min_count):
 
         self.data = DataReader(input_file, min_count)
         dataset = Word2vecDataset(self.data, window_size)
@@ -38,7 +181,7 @@ class Word2VecTrainer:
             scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(self.dataloader))
 
             running_loss = 0.0
-            for i, sample_batched in enumerate(tqdm(self.dataloader)):
+            for i, sample_batched in enumerate(self.dataloader):
 
                 if len(sample_batched[0]) > 1:
                     pos_u = sample_batched[0].to(self.device)
@@ -59,5 +202,21 @@ class Word2VecTrainer:
 
 
 if __name__ == '__main__':
-    w2v = Word2VecTrainer(input_file="input.txt", output_file="out.vec")
-    w2v.train()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_file', help = 'text file for training data', required = True)
+    parser.add_argument('--output_file', help = 'file location for trained embeddings', required = True)
+    parser.add_argument('--emb_dimension', help = 'dimension of trained embedding', default = 100, required = False)
+    parser.add_argument('--batch_size', default = 32, required = False)
+    parser.add_argument('--window_size', help = 'training window size', default = 5, required = False)
+    parser.add_argument('--iterations', help = 'number of training iterations', default = 10, required = False)
+    parser.add_argument('--initial_lr', help = 'starting learning rate, to be updated by sparse_adam optimizer', default = 0.001, required = False)
+    parser.add_argument('--min_count', help = 'minimum word frequency for inclusion', default = 12, required = False)
+    #sagemaker container environment
+    parser.add_argument("--hosts", type=list, default=json.loads(os.environ["SM_HOSTS"]))
+    parser.add_argument("--current-host", type=str, default=os.environ["SM_CURRENT_HOST"])
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_TRAINING"])
+    parser.add_argument("--num-gpus", type=int, default=os.environ["SM_NUM_GPUS"])
+    args = vars(parser.parse_args())
+    w2v = Word2VecTrainer(**args)
+    w2v.train()  
